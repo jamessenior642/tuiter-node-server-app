4c2309eaa21e000e1ee9f528135f22196f3b7a23
jamessenior642 <jvsenior02@gmail.com>
2023-04-11 15:41:47 -0400

    a9

--- app.js
+++ app.js
@@ -11,4 +11,7 @@ HelloController(app);
 UserController(app);
 TuitsController(app);
 app.listen(process.env.PORT || 4000);
-
+import mongoose from "mongoose";
+// mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://172.31.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
