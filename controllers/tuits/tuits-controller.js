c97a63b31f7f487a03b988e443387ae6cc437fe3
jamessenior642 <jvsenior02@gmail.com>
2023-04-11 12:40:27 -0400

    a8 fix

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,17 +1,45 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+const currentUser = {
+    "userName": "NASA",
+    "handle": "@nasa",
+    "image": "nasa-icon.png",
+};
+
+// eslint-disable-next-line no-unused-vars
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "disliked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+    "dislikes": 0,
+}
+
 const createTuit = (req, res) => {
-    const newTuit = req.body;
+    let newTuit = req.body;
+    newTuit = {...templateTuit, ...newTuit};
     newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
     newTuit.liked = false;
+    newTuit.dislikes = 0;
+    newTuit.disliked = false;
     tuits.push(newTuit);
     res.json(newTuit);
 }
 
-const findTuits = (req, res) =>
-    res.json(tuits);
 const updateTuit = (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
@@ -22,6 +50,7 @@ const updateTuit = (req, res) => {
     res.sendStatus(200);
 }
 
+
 const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
     tuits = tuits.filter((t) =>
@@ -30,9 +59,5 @@ const deleteTuit = (req, res) => {
 }
 
 
-export default (app) => {
-    app.post('/api/tuits', createTuit);
-    app.get('/api/tuits', findTuits);
-    app.put('/api/tuits/:tid', updateTuit);
-    app.delete('/api/tuits/:tid', deleteTuit);
-}
+const findTuits = (req, res) =>
+    res.json(tuits);
--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,51 @@
-export default [
+const posts = [
     {
-        "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
-        "title": "SpaceX's Mission",
-        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+        "_id": 123,
+        "topic": "Space",
+        "userName": "SpaceX",
+        "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
+        "time": "2h",
+        "image": "tesla-icon.png",
+        "liked": true,
+        "disliked": true,
+        "replies": 123,
+        "retuits": 543,
+        "likes": 1234,
+        "dislikes": 234,
+        "handle": "@spacex",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
     },
     {
-        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-        "title": "The Boring Company fixing traffic",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345, "replies": 321, "retuits": 432
+        "_id": 234,
+        "topic": "Space",
+        "userName": "SpaceX",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "time": "2h",
+        "image": "spacex-icon.png",
+        "liked": false,
+        "disliked": false,
+        "replies": 323,
+        "retuits": 643,
+        "likes": 1754,
+        "dislikes": 2342,
+        "handle": "@spacex",
+        "tuit": "The first ever 3D-printed, methane-fueled rocket will attempt to launch into orbit from Cape Canaveral in Florida on Wednesday. The 33.5 metre (110 foot) Terran 1 rocket, built by California-based startup Relativity Space, uses 3D printed engines and a 3D printed structure to lift up to 1,250 kilograms into low-Earth orbit."
     },
     {
-        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-        "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
-        "title": "Neurolink fixes paralisis and blindness",
-        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
-    },
-];
+        "_id": 345,
+        "topic": "Energy",
+        "userName": "Tesla",
+        "title": "Tesla FSD finally makes driving obsolete. After years of delay, Tesla Full Self Driving can drive you to work while you work, play, or sleep",
+        "time": "4d",
+        "image": "tesla-icon.png",
+        "liked": true,
+        "disliked": false,
+        "replies": 727,
+        "retuits": 7227,
+        "likes": 9789,
+        "dislikes": 535,
+        "handle": "@tesla",
+        "tuit": "Tesla (NASDAQ: TSLA) shares rose over 5% in Tuesday trading and held onto a gain of 3.6% as of 3:40 p.m. ET. The move came after a vote of confidence from one widely followed tech investor and as CEO Elon Musk chimed in on a report about the company's battery supply."
+    }
+]
+export default posts;
